379ffefea07b7d4cd56956a48da7166d596c2bae
ZachariasAnastasakis <zaxarisanastasakis@gmail.com>
2022-05-31 16:54:42 +0300

    added tensorboards

--- train.py
+++ train.py
@@ -4,19 +4,24 @@ from torchvision import datasets, transforms
 import matplotlib.pyplot as plt
 from tqdm import tqdm
 import torch.optim as optim
+from torch.utils.tensorboard import SummaryWriter
 from prepare_dataset import LoadDataset
 from Autoencoder import Autoencoder
 # add tensor boards
+writer = SummaryWriter('runs/AutoEncoder_train_loss')
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 data = LoadDataset(dataset="MNIST")
 data_loader = data.DataLoader()
+total_steps = len(data_loader)
 
 autoencoder = Autoencoder()
 epochs = 20
 criterion = nn.MSELoss()
 optimizer = optim.Adam(autoencoder.parameters(), lr=0.001, weight_decay=1e-5)
 
+running_loss = 0
+i = 0
 for epoch in range(epochs):
     for image, label in tqdm(data_loader, desc=f'Epoch {epoch}/{epochs}'):
         image = image.reshape(-1, 28 * 28)
@@ -26,6 +31,9 @@ for epoch in range(epochs):
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
+
+        #running_loss += loss.item()
+        writer.add_scalar('Loss/Train', loss.item(), epoch * total_steps + 1)
 
 print(f"Training finished with loss: {loss.item()}")
 torch.save(autoencoder, './model.pth')
